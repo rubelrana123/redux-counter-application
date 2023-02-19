2b2bd69db3879d9bef81e73eb792865ba1aa7db1
rubelrana123 <rubelranatpi707458@gmail.com>
2023-02-20 04:02:17 +0600

    action creator and identi

--- index.js
+++ index.js
@@ -8,17 +8,35 @@ const initialState = {
     value: 0,
 };
 
+//action identifiar
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+//action creator
+const incrementFn = (value) => {
+   
+      return{
+        type : INCREMENT,
+        payload : value
+      }
+  
+}
+const decrementFn = (value) => {
+   return{
+    type: DECREMENT,
+    payload : value
+   }
+}
 // create reducer function
 function counterReducer(state = initialState, action) {
     if (action.type === "increment") {
         return {
             ...state,
-            value: state.value + 1,
+            value: state.value + action.payload,
         };
     } else if (action.type === "decrement") {
         return {
             ...state,
-            value: state.value - 1,
+            value: state.value - action.payload,
         };
     } else {
         return state;
@@ -40,13 +58,11 @@ store.subscribe(render);
 
 // button click listeners
 increment.addEventListener("click", () => {
-    store.dispatch({
-        type: "increment",
-    });
+    store.dispatch(incrementFn(2));
 });
 
 decrement.addEventListener("click", () => {
-    store.dispatch({
-        type: "decrement",
-    });
+    store.dispatch( 
+       decrementFn(5)
+     );
 });
